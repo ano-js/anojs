f94d0a7e9e0d1f1a3ca185ded6966c3680f277a8
Calix Huang <calix.huang1@gmail.com>
2020-03-03 21:41:06 -0800

    two new animations + images

---
+++ animation-files/anojs-continuous-random-burst.js
@@ -0,0 +1,60 @@
+// calixo888
+
+function randomBurst() {
+  for (var i = 0; i < 10; i++) {
+    var randomCoords = {
+      x: Math.random() * ((Math.random() * parentContainer.width() - (parentContainer.width() / 2))),
+      y: Math.random() * ((Math.random() * parentContainer.height() - (parentContainer.height() / 2))),
+    }
+
+    burst = new mojs.Burst(burstConstructor).tune(randomCoords);
+    burst2 = new mojs.Burst(burst2Constructor).tune(randomCoords);
+    circle = new mojs.Shape(circleConstructor).tune(randomCoords);
+
+    new mojs.Timeline().add(burst, burst2, circle).play();
+  }
+}
+
+
+const burstConstructor = {
+  radius: { 0: 100 },
+  count: 20,
+  children: {
+    shape: "cross",
+    stroke: "teal",
+    strokeWidth: { 6: 0 },
+    angle: { 360: 0 },
+    radius: { 20: 10 },
+    duration: 3000
+  }
+};
+
+const burst2Constructor = {
+  radius: { 0: 300 },
+  count: 18,
+  children: {
+    shape: "zigzag",
+    stroke: { "cyan": "orange" },
+    opacity: { 1: 0 },
+    fill: "none",
+    strokeWidth: { 6: 0 },
+    angle: { 0: 360 },
+    radius: { 20: 10 },
+    duration: 3000
+  }
+};
+
+const circleConstructor = {
+  radius: { 0: 400 },
+  fill: "none",
+  stroke: "teal",
+  duration: 3000,
+  opacity: {1:0},
+};
+
+const parentContainer = $("#anojs-random-burst");
+
+randomBurst();
+setInterval(function() {
+  randomBurst();
+}, 3000);
