3912092642ddf031746b58ca3d60856875d884d2
Code Jabo <44330015+jabo-bernardo@users.noreply.github.com>
2020-04-07 15:10:22 +0800

    🔨 Added Colorful Samurai Animation

---
+++ animation-files/anojs-colorful-samurai.js
@@ -0,0 +1,77 @@
+let canvasDiv = document.querySelector("#anojs-mouse-trail");
+canvasDiv.innerHTML += "<canvas id='anojs-mouse-trail-canvas'></canvas>";
+let canvas = document.querySelector("#anojs-mouse-trail-canvas");
+
+canvas.style.width = '100%';
+canvas.style.height = '100%';
+canvas.width = canvas.offsetWidth;
+canvas.height = canvas.offsetHeight;
+
+let c = canvas.getContext("2d");
+
+class MouseData {
+    constructor() {
+        this.mouseLastPosition = [];
+
+        this.popCounter = 0;
+    }
+
+    update() {
+        if(this.mouseLastPosition.length > 10) {
+            this.mouseLastPosition.pop();
+        }
+    }
+
+    render() {
+
+        c.beginPath();
+        if(this.mouseLastPosition[10] != undefined) {
+            c.moveTo(this.mouseLastPosition[10].x, this.mouseLastPosition[10].y);
+        }
+        for(let i = 0; i < 10; i++) {
+            if(this.mouseLastPosition[i] != undefined) {
+                c.lineTo(this.mouseLastPosition[i].x,this.mouseLastPosition[i].y);
+            }
+        }
+        c.fillStyle = "orange";
+        c.fill();
+
+    }
+}
+
+let mouse = new MouseData();
+
+let animationLoop = function() {
+
+    c.fillStyle = 'rgb(10, 10, 10)';
+    c.fillRect(0, 0, canvas.width, canvas.height);
+
+    mouse.update();
+    mouse.render();
+
+    requestAnimationFrame(animationLoop);
+
+}
+
+animationLoop();
+
+document.addEventListener('mousemove', e => {
+
+    let mousePos = getMousePosition(e);
+    mouse.mouseLastPosition.unshift(mousePos);
+
+}, false)
+
+
+// Misc
+let getMousePosition = function(e) {
+    let cnv = canvas.getBoundingClientRect();
+    return {
+        x: e.clientX - cnv.left,
+        y: e.clientY - cnv.top
+    };
+}
+
+let Distance = function(x1, y1, x2, y2) {
+    return Math.sqrt( Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2) );
+}
